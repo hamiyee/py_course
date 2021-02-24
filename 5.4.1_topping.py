abacc359ada473591edc29053e04e830f4706f99
hamiyee <hmj3632@163.com>
2021-02-24 20:51:03 +0800

    *

---
+++ 5.4.1_topping.py
@@ -0,0 +1,7 @@
+request_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for request_topping in request_toppings:
+    if request_topping == 'green peppers':
+        print('Sorry, we are out of green peppers right now.')
+    else:
+        print(f'\nFinished making your pizza!')
